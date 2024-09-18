8c7c33fad1438da05b766c24545bced72cbc995f
vladimirbutyrskii <vlaver@mail.ru>
2024-09-18 20:31:46 +0300

    3-rd commit

---
+++ config/urls.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('catalog.urls', namespace='catalog')),
+    path('myblog/', include('myblog.urls', namespace='myblog'))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
